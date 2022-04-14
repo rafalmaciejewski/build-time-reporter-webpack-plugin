f0499826e0d82b91a74ed97065358d6a4fbf9eaf
Rafał Maciejewski <rafaljestsuper@gmail.com>
2022-04-14 15:27:25 +0200

    fix: fix module exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 import { BuildTimeReporterWebpackPlugin } from './BuildTimeReporterWebpackPlugin';
 
-import { BuildTimeReporterWebpackPluginOptions, StepStats, AssetStats, Step, BuildTimeReporterStats } from './types';
-
-export type { BuildTimeReporterWebpackPluginOptions, StepStats, AssetStats, Step, BuildTimeReporterStats };
-
-export default BuildTimeReporterWebpackPlugin;
-
-export { BuildTimeReporterWebpackPlugin };
+export = BuildTimeReporterWebpackPlugin;
